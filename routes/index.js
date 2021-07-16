600c0b265a1451333d7133b78d017b9762dc23b9
harshvijaykumarrawat <59377014+harshvijaykumarrawat@users.noreply.github.com>
2021-07-16 18:02:42 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const resume_json = require(__dirname+'\\resume.json');
+const resume_json = require(__dirname+'/resume.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
